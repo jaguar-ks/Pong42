20534d0f41a79ad518e7efbc03871c93cd79c95f
Zakaria El Bouzkri <elbouzkri9@gmail.com>
2024-09-29 19:27:28 +0100

    add drf_spectacular configuration to api

---
+++ srcs/api/config/settings/drf_spectacular.py
@@ -0,0 +1,11 @@
+
+
+
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'transcendance',
+    'DESCRIPTION': '42 transcendance project API ',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+}
+
