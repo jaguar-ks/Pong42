49ed7cdd050f543d6279c4ac35fcb4fd7e75e3fb
Zakaria El Bouzkri <elbouzkri9@gmail.com>
2024-09-29 19:28:11 +0100

    import settings files to base settings module

--- srcs/api/config/django/base.py
+++ srcs/api/config/django/base.py
@@ -115,3 +115,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+from config.settings.rest_framework import *
+from config.settings.simplejwt import *
+from config.settings.drf_spectacular import *
