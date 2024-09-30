faf79a722b17929cbd1c30ff81748119d1965406
Zakaria El Bouzkri <elbouzkri9@gmail.com>
2024-09-30 02:21:00 +0100

    add: Enable/Disable Views

---
+++ srcs/api/apps/authentication/views/two_factor.py
@@ -0,0 +1,24 @@
+from rest_framework.generics import GenericAPIView
+from rest_framework.response import Response
+
+from apps.authentication.serializers.two_factor import TwoFASerializer
+
+class   TwoFaBaseView(GenericAPIView):
+    serializer_class = TwoFASerializer
+    context = {'action': 'enable'}
+    
+    def post(self, request):
+        self.context['request'] = request
+        serializer = self.serializer_class(
+            data=request.data,
+            context=self.context,
+        )
+        serializer.is_valid(raise_exception=True)
+        return Response(serializer.data)
+
+class   Enable2FaView(TwoFaBaseView):
+    context = {'action': 'disable'}
+
+class   Disable2FaView(TwoFaBaseView):
+    context = {'action': 'disable'}
+
