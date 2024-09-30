fdfcfb2c79a389d784093700debf201f3e40ed98
Zakaria El Bouzkri <elbouzkri9@gmail.com>
2024-09-30 03:48:26 +0100

    add AuthenticationAppConfig

---
+++ srcs/api/apps/authentication/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+class   AuthenticationAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.authentication'
+    
+    def ready(self) -> None:
+        import apps.authentication.signals
