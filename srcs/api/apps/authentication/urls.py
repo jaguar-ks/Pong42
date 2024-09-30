703ddc1ca722f97a0a1b12e1472754f934046837
Zakaria El Bouzkri <elbouzkri9@gmail.com>
2024-09-30 02:19:54 +0100

    add 2FA urls in auth urlpatterns

--- srcs/api/apps/authentication/urls.py
+++ srcs/api/apps/authentication/urls.py
@@ -6,9 +6,15 @@ from rest_framework_simplejwt.views import (
     TokenVerifyView
 )
 
+from .views import two_factor
+
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('token/blacklist/', TokenBlacklistView.as_view(), name='token_blacklist'),
+    
+    # 2Fa
+    path('2fa/enable/', two_factor.Enable2FaView.as_view(), name='enable_2fa'),
+    path('2fa/disable/', two_factor.Enable2FaView.as_view(), name='disable_2fa'),
 ]
