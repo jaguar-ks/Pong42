be1dc9c2503ad0e60c49a2305bc8ddec5af6df56
Zakaria El Bouzkri <elbouzkri9@gmail.com>
2024-10-13 06:39:48 +0100

    add games statistics field to AuthUser

--- srcs/api/apps/users/models.py
+++ srcs/api/apps/users/models.py
@@ -3,3 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 class   User(AbstractUser):
     avatar_url = models.URLField(max_length=200, null=True, blank=True)
+    wins = models.PositiveSmallIntegerField(default=0)
+    loses = models.PositiveSmallIntegerField(default=0)
+    rating = models.PositiveIntegerField(default=0)
+    rank = models.PositiveIntegerField(default=500)
