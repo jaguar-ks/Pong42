ee9f2ef8cd8d0bef5518c6372026fef8ca61f20a
Zakaria El Bouzkri <elbouzkri9@gmail.com>
2024-10-13 08:15:55 +0100

    setup development asgi server with channels

--- srcs/api/config/django/base.py
+++ srcs/api/config/django/base.py
@@ -12,6 +12,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -64,6 +65,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
+ASGI_APPLICATION = "config.asgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
