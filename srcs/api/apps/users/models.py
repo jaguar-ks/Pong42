43582d8d766feadb25a9cc9aa0d2ced28b57ba16
Zakaria El Bouzkri <elbouzkri9@gmail.com>
2024-10-13 06:21:37 +0100

    add avatar_url field to authuser

--- srcs/api/apps/users/models.py
+++ srcs/api/apps/users/models.py
@@ -2,4 +2,4 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class   User(AbstractUser):
-    pass
+    avatar_url = models.URLField(max_length=200, null=True, blank=True)
