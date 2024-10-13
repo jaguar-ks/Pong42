bcc9b3fa8d78bc1ad266cb9c04b63328b7deb1c3
Zakaria El Bouzkri <elbouzkri9@gmail.com>
2024-10-13 06:38:10 +0100

    set default permissions classes as IsAuthenticaed since most of views require authentication

--- srcs/api/config/settings/rest_framework.py
+++ srcs/api/config/settings/rest_framework.py
@@ -4,5 +4,8 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',  # Example: only authenticated users can access the API
+    ],
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
