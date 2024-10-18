4e377af6308aaaf31eb1e1f89743c234a07fc1c3
Zakaria El bouzkri <elbouzkri9@gmail.com>
2024-10-18 03:10:11 +0100

    fix AuthUpdateSerializer

--- srcs/api/apps/utils/validators.py
+++ srcs/api/apps/utils/validators.py
@@ -41,28 +41,3 @@ class EmailValidator:
             raise serializers.ValidationError("A user with that email already exists.")
 
 
-# class SignUpSerializer(serializers.Serializer):
-#     id = serializers.IntegerField(read_only=True)
-#     username = serializers.CharField(validators=[UsernameValidator()])
-#     email = serializers.EmailField(validators=[EmailValidator()])
-#     password = serializers.CharField(write_only=True, validators=[PasswordValidator()])
-#     first_name = serializers.CharField(validators=[NameValidator("First name")])
-#     last_name = serializers.CharField(validators=[NameValidator("Last name")])
-
-#     def validate(self, data):
-#         if data.get('first_name') and data.get('last_name'):
-#             if data['first_name'].lower() == data['last_name'].lower():
-#                 raise serializers.ValidationError("First name and last name should not be the same.")
-#         return data
-
-#     def create(self, validated_data):
-#         return User.objects.create_user(**validated_data)
-
-#     def update(self, instance, validated_data):
-#         for attr, value in validated_data.items():
-#             if attr == 'password':
-#                 instance.set_password(value)
-#             else:
-#                 setattr(instance, attr, value)
-#         instance.save()
-#         return instance
