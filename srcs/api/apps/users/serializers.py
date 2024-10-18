495e74e28feead3e9aa283738d3c3c3d1f948c85
Zakaria El Bouzkri <zel-bouz@e1r1p3.1337.ma>
2024-10-18 03:10:11 +0100

    fix AuthUpdateSerializer

--- srcs/api/apps/users/serializers.py
+++ srcs/api/apps/users/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from apps.utils import validators
 
 class   AuthUserSerializer(serializers.ModelSerializer):
     class   Meta:
@@ -9,3 +10,20 @@ class   AuthUserSerializer(serializers.ModelSerializer):
 class   UpdateAuthUserSerializer(serializers.ModelSerializer):
     class   Meta:
         model = get_user_model()
+        fields = ('id', 'first_name', 'last_name', \
+                    'username', 'email', 'avatar_url', 'password')
+        extra_kwargs = {
+            'username': {'validators': [validators.UsernameValidator()]},
+            'password': {
+                'write_only': True,
+                'validators': [validators.PasswordValidator()]
+            },
+            'email': {'validators': [validators.EmailValidator()]},
+            'first_name': {'validators': [validators.NameValidator('First name')]},
+            'last_name': {'validators': [validators.NameValidator('Last name')]},
+        }
+    
+    def update(self, instance, validated_data):
+        if 'password' in validated_data:
+            instance.set_password(validated_data.pop('password'))
+        return super().update(instance, validated_data)
--- srcs/api/apps/users/views.py
+++ srcs/api/apps/users/views.py
@@ -1,12 +1,18 @@
 from rest_framework import generics, filters
-from .serializers import AuthUserSerializer
 from rest_framework.permissions import AllowAny
 from .models import User
 from rest_framework.response import Response
 
+from . import serializers
 
 class   AuthUserView(generics.RetrieveUpdateDestroyAPIView):
-    serializer_class = AuthUserSerializer
+
+    def get_serializer_class(self):
+        if self.request.method in ['PUT', 'PATCH']:
+            return serializers.UpdateAuthUserSerializer
+        return serializers.AuthUserSerializer
+
+        return super().get_serializer_class()
     def destroy(self, request, *args, **kwargs):
         request.user.is_active = False
         request.user.save()
@@ -15,14 +21,15 @@ class   AuthUserView(generics.RetrieveUpdateDestroyAPIView):
     def get_object(self):
         return self.request.user
 
+
 class   ListUserView(generics.ListAPIView):
-    serializer_class = AuthUserSerializer
+    serializer_class = serializers.AuthUserSerializer
 
     def get_queryset(self):
         return User.objects.filter(is_active=True)
 
 class LeaderBoardView(generics.ListAPIView):
-    serializer_class = AuthUserSerializer
+    serializer_class = serializers.AuthUserSerializer
 
     def get_queryset(self):
         return User.objects.filter(is_active=True).order_by('-rating')
@@ -30,7 +37,7 @@ class LeaderBoardView(generics.ListAPIView):
 class   UserSearchView(generics.ListAPIView):
     queryset = User.objects.filter(is_active=True)
     filter_backends = [filters.SearchFilter]
-    serializer_class = AuthUserSerializer
+    serializer_class = serializers.AuthUserSerializer
     search_fields = ['username', 'email', 'first_name', 'last_name']
 
 
