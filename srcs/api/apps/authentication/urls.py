01fe9115c2d17a1c2d2451422eb3ba393856eeb4
ZakariaElbouzkri <elbouzkri9@gmail.com>
2024-11-19 19:52:44 +0100

    feat: Add email verification and sign-in functionality

--- srcs/api/apps/authentication/urls.py
+++ srcs/api/apps/authentication/urls.py
@@ -10,4 +10,8 @@ urlpatterns = [
     path('sign-up/', views.SignUpView.as_view(), name='sign_up'),
     path('sign-in/', views.SignInView.as_view(), name='sign_in'),
     path('sign-out/', views.SignOutView.as_view(), name='sign_out'),
+
+    path('email/verify/<str:uid>/<str:token>/', views.EmailVerifyView.as_view(), name='verify_email'),
+    path('email/sign-in/<str:uid>/<str:token>/', views.EmailSignInView.as_view(), name='email_sign_in' ),
+    path('email/send_email/', views.SendEmailView.as_view(), name='send_email'),
 ]
