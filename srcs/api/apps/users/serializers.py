1d1726e6ff4f21d396396ed766bc025635a5735d
ZakariaElbouzkri <elbouzkri9@gmail.com>
2024-11-24 07:49:38 +0100

    feat: Add ConnectionSerializer for managing user connections

--- srcs/api/apps/users/serializers.py
+++ srcs/api/apps/users/serializers.py
@@ -53,3 +53,74 @@ class   UpdateAuthUserSerializer(serializers.ModelSerializer):
         if 'password' in validated_data:
             instance.set_password(validated_data.pop('password'))
         return super().update(instance, validated_data)
+
+
+class ConnectionSerializer(serializers.Serializer):
+    # Input
+    recipient = serializers.IntegerField(write_only=True, required=True)
+
+    # Output
+    id = serializers.IntegerField(read_only=True)
+    user = UserSerializer(read_only=True)
+    status = serializers.CharField(read_only=True)
+    created_at = serializers.DateTimeField(read_only=True)
+    updated_at = serializers.DateTimeField(read_only=True)
+
+    def validate_recipient(self, value):
+        user = self.context['request'].user
+        try:
+            if user.id == value:
+                raise serializers.ValidationError("You cannot connect with yourself")
+
+            recipient = User.objects.get(id=value)
+
+            # Check existing connection
+            connection_exists = Connection.objects.filter(
+                (Q(initiator=user) & Q(recipient=recipient)) |
+                (Q(initiator=recipient) & Q(recipient=user))
+            ).exists()  # Added .exists() for efficiency
+
+            if connection_exists:
+                raise serializers.ValidationError('Connection already exists with this user')
+
+            self.context['recipient'] = recipient
+            return value
+
+        except User.DoesNotExist:
+            raise serializers.ValidationError("User does not exist")
+
+    def to_representation(self, instance):
+        request = self.context.get('request')
+        if not request:
+            return {}
+
+        user = instance.get_other_user(request.user)
+        user_data = (
+            UserSerializer(user).data if instance.status != Connection.BLOCKED
+            else {'username': user.username}
+        )
+
+        return {
+            'id': instance.id,
+            'user': user_data,
+            'status': instance.status,
+            'created_at': instance.created_at,
+            'updated_at': instance.updated_at,
+        }
+
+    def create(self, validated_data):
+        return Connection.objects.create(
+            initiator=self.context['request'].user,
+            recipient=self.context['recipient'],
+        )
+
+
+class   BlockConnectionSerializer(serializers.Serializer):
+    user_id = serializers.IntegerField(write_only=True, required=True)
+
+    def validate(self, attrs):
+        user = self.context['request'].user
+
+        if user.id == attrs['user_id'].id:
+            raise serializers.ValidationError("You cannot block with yourself")
+        
