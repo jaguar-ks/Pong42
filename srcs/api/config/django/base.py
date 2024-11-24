356af308524048779964a88969d3e38b3141cb39
ZakariaElbouzkri <elbouzkri9@gmail.com>
2024-11-24 07:50:26 +0100

    chore: Add 'jazzmin' app to INSTALLED_APPS in Django config

--- srcs/api/config/django/base.py
+++ srcs/api/config/django/base.py
@@ -14,6 +14,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'daphne',
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -46,7 +47,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
 
     'corsheaders.middleware.CorsMiddleware',
-    
+
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
