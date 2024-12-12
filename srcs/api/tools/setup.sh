c1659c6a4d957c4747bc0b5eb055e011aa2c14bf
jaguar-ks <fahdamineksouss@gmail.com>
2024-12-12 02:10:17 +0100

    FIX : extracting the credentionls successfully

--- srcs/api/tools/setup.sh
+++ srcs/api/tools/setup.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 set -e  # Exit immediately if a command exits with a non-zero status
 
+apk add jq
+
+if [ ! -e cred.env ]; then
+    echo "ROLE_ID=\"$(cat django-cred.json |  jq -r '.role_id')\"" >> cred.env
+    echo "SECRET_ID=\"$(cat django-cred.json |  jq -r '.secret_id')\"" >> cred.env
+fi
+
+source cred.env
+export ROLE_ID=$ROLE_ID
+export SECRET_ID=$SECRET_ID
+# export ROLE_ID=$(cat django-cred.json |  jq -r '.role_id')
+# export SECRET_ID=$(cat django-cred.json |  jq -r '.secret_id')
+
 # Apply migrations
 python3 manage.py makemigrations
 python3 manage.py migrate
