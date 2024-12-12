1f3f631e75eaf93555bb9d6ffb0507ba5c56f026
IsmailBarka <ismailbarka82@gmail.com>
2024-12-12 06:09:37 +0100

    chore: Add lazy loading for user not found image

--- srcs/frontend/src/app/users/search/[id]/page.tsx
+++ srcs/frontend/src/app/users/search/[id]/page.tsx
@@ -12,11 +12,14 @@ import Friends from "@/components/Friends/Friends";
 import { Achievements } from "@/components/Achievements/Achievements";
 import classes from "./page.module.css";
 import FriendsFR from "@/components/FriendsFR/FriendsFR";
+import Image from "next/image";
+import userNotFoundImage from '../../../../../assets/userNotFound.svg'
 
 const SearchProfile = () => {
   const { updateSearchedUserData, updateCurrentPage, searchedUserData, updateUserDataSearch } = useUserContext();
   const { id } = useParams();
   const [isLoading, setIsLoading] = useState(true);
+  const [userExist, setUserExist] = useState(false);
 
   useEffect(() => {
     updateCurrentPage("");
@@ -32,7 +35,13 @@ const SearchProfile = () => {
         updateSearchedUserData(res.data);
         updateUserDataSearch(res.data);
       } catch (err) {
-        console.error("Error fetching user data:", err);
+        console.error("Error fetching user data:", err.response.data.detail);
+        if(err?.response?.data?.detail)
+          {
+            console.log(err?.response?.data?.detail);
+            
+          }
+          setUserExist(true);
       } finally {
         setIsLoading(false);
       }
@@ -45,7 +54,7 @@ const SearchProfile = () => {
     <div className={classes.home}>
       {isLoading ? (
         <div>Loading...</div>
-      ) : (
+      ) : !userExist ? (
         <div className={classes.container}>
           <div className={classes.box1}>
             <PlayerInfos user="search" />
@@ -60,12 +69,20 @@ const SearchProfile = () => {
             <ProgressBar user="search" />
           </div>
           <div className={classes.box4}>
-            <FriendsFR user="search" />
+            <FriendsFR id={id} />
           </div>
           <div className={classes.box5}>
             <Achievements user="search" />
           </div>
           <div className={classes.box6}>Line 6</div>
+        </div>
+      ) : (
+        <div className={classes.notFoundContainer}>
+          <Image width={100} height={100} src={userNotFoundImage} alt="user not found" className={classes.userNotFoundImage}/>
+          <p className={classes.notFoundMessage}>This user does not exist.</p>
+          <button className={classes.homeButton} onClick={() => window.location.href = '/users/home'}>
+            Go to Home
+          </button>
         </div>
       )}
     </div>
