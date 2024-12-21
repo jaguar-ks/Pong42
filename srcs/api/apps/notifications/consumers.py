86ca0f3ff42a0f0f69ddcc8eb08b47b2fd8236a8
jaguar-ks <fahdamineksouss@gmail.com>
2024-12-21 14:31:19 +0100

    FIX : checking if the user is authenticated before connecting to the socket and checking if the group name attr exists before discarding it in the disconnection

--- srcs/api/apps/notifications/consumers.py
+++ srcs/api/apps/notifications/consumers.py
@@ -4,22 +4,24 @@ from django.forms.models import model_to_dict
 
 class NotificationConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.user_id = self.scope['url_route']['kwargs']['user_id']
-        self.group_name = f'user_{self.user_id}'
-
-        # Add user to their group
-        await self.channel_layer.group_add(
-            self.group_name,
-            self.channel_name
-        )
-        await self.accept()
+        user = self.scope['user']
+        if user.is_authenticated:
+            self.group_name = f"user_{user.id}"
+            await self.channel_layer.group_add(
+                self.group_name,
+                self.channel_name
+            )
+            await self.accept()
+        else:
+            await self.close()
 
     async def disconnect(self, close_code):
         # Remove user from the group
-        await self.channel_layer.group_discard(
-            self.group_name,
-            self.channel_name
-        )
+        if hasattr(self, 'group_name'):
+            await self.channel_layer.group_discard(
+                self.group_name,
+                self.channel_name
+            )
 
     async def send_notification(self, event):
         # Send notification to WebSocket
