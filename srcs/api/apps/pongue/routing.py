d8947c375221de38ec79b8044fca02bedd481b8e
ZakariaElbouzkri <elbouzkri9@gmail.com>
2024-12-28 08:45:49 +0100

    chore: remove routing files and append websockets routes directly in the URLRouter constractor

--- srcs/api/apps/pongue/routing.py
+++
@@ -1,7 +0,0 @@
-from django.urls import re_path
-
-from . import consumers
-
-ws_urlpatterns = [
-    re_path('ws/pongue/$', consumers.PongueConsumer.as_asgi()),
-]
--- srcs/api/config/asgi.py
+++ srcs/api/config/asgi.py
@@ -3,15 +3,26 @@ import os
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from config.middleware import WebSocketJWTMiddleware
-from apps.users import routing    
 from channels.security.websocket import AllowedHostsOriginValidator
-# from apps.chat import routing
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.local")
+
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
 
-from apps.pongue.routing import ws_urlpatterns as pongue_urlpatterns
+from django.urls import re_path
+from apps.pongue.consumers import PongueConsumer
+from apps.users.consumers import ChatConsumer
+
+async def unmatched_route(scope, receive, send):
+    await send(
+        {
+            "type": "websocket.close",
+            "code": 4004,
+        }
+    )
+
 
 application = ProtocolTypeRouter(
     {
@@ -20,8 +31,9 @@ application = ProtocolTypeRouter(
             WebSocketJWTMiddleware(
                 URLRouter(
                     [
-                        *pongue_urlpatterns +
-                        routing.websocket_urlpatterns
+                        re_path('ws/pongue/$', PongueConsumer.as_asgi()),
+                        re_path(r'ws/chat/$', ChatConsumer.as_asgi()),
+                        re_path(r'', unmatched_route), # catch all unmatched routes
                     ]
                 )
             )
