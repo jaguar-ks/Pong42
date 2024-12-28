d4eb60c73c54b03ca563786d2088b4e59fc6dcc9
ZakariaElbouzkri <elbouzkri9@gmail.com>
2024-12-28 08:20:42 +0100

    chore: update the consumer and route for game websocket

--- srcs/api/apps/pongue/routing.py
+++ srcs/api/apps/pongue/routing.py
@@ -3,5 +3,5 @@ from django.urls import re_path
 from . import consumers
 
 ws_urlpatterns = [
-    re_path(r"^ws/match/(?P<match_id>\d+)/$", consumers.MatchConsumer.as_asgi())
+    re_path('ws/pongue/$', consumers.PongueConsumer.as_asgi()),
 ]
