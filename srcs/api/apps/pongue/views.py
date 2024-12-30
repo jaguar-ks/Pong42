3dd5f29b7fc935b9c2da537ef90a4464a3aa2728
ZakariaElbouzkri <elbouzkri9@gmail.com>
2024-12-30 20:17:27 +0100

    feat: added user tournaments history view

--- srcs/api/apps/pongue/views.py
+++ srcs/api/apps/pongue/views.py
@@ -4,34 +4,47 @@ from rest_framework.generics import ListAPIView, RetrieveAPIView
 from .serializers import (
     MatchSerializer,
     TournamentSerializer,
-    TournamentDetailSerializer
+    TournamentDetailSerializer,
 )
 from .models import (
     PongMatch,
     Tournament,
 )
 
-class   MatchHistoryView(ListAPIView):
+
+class MatchHistoryView(ListAPIView):
     serializer_class = MatchSerializer
     queryset = PongMatch.objects.all()
 
 
-class   TournamentHistoryView(ListAPIView):
+class TournamentHistoryView(ListAPIView):
     serializer_class = TournamentSerializer
     queryset = Tournament.objects.all()
 
 
-class   TournamentDetailView(RetrieveAPIView):
+class TournamentDetailView(RetrieveAPIView):
     serializer_class = TournamentDetailSerializer
     queryset = Tournament.objects.all()
-    lookup_url_kwarg = 'tournament_id'
+    lookup_url_kwarg = "tournament_id"
 
 
-class   UserMatchHistoryView(ListAPIView):
+class UserMatchHistoryView(ListAPIView):
     serializer_class = MatchSerializer
 
     def get_queryset(self):
-        user_id = self.kwargs.get('user_id')
-        return PongMatch.objects.filter(
-            participants__user_id=user_id
-        ).order_by('-played_at')
+        user_id = self.kwargs.get("user_id")
+        return PongMatch.objects.filter(participants__user_id=user_id).order_by(
+            "-played_at"
+        )
+
+
+class UserTournamentHistoryView(ListAPIView):
+    serializer_class = TournamentSerializer
+
+    def get_queryset(self):
+        user_id = self.kwargs.get("user_id")
+        return (
+            Tournament.objects.filter(participants__user_id=user_id)
+            .select_related('winner', 'creator')
+            .order_by("-ended_at", "-created_at")
+        )
