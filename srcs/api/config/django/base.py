1c6e157803b9eb3f4b538bedc2fb0d19461f13f9
jaguar-ks <fahdamineksouss@gmail.com>
2024-12-31 19:12:27 +0100

    UPDATE : moved the database and the channel layers config to the local and the cors config to the base

--- srcs/api/config/django/base.py
+++ srcs/api/config/django/base.py
@@ -83,21 +83,15 @@ TEMPLATES = [
 
 ASGI_APPLICATION = "config.asgi.application"
 
-# Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+CORS_ALLOW_CREDENTIALS = True
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://127.0.0.1:8000",
+]
 
-CHANNEL_LAYERS = {
-    "default": {
-        "BACKEND": "channels.layers.InMemoryChannelLayer"
-    }
-}
+ALLOWED_HOSTS = ['*']
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
