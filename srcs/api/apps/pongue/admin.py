f043c0fac7f808e39bae8b7eb1d3e7f423da7158
Zakaria El Bouzkri <elbouzkri9@gmail.com>
2025-01-20 19:18:37 +0100

    add: register match model in django admin

--- srcs/api/apps/pongue/admin.py
+++ srcs/api/apps/pongue/admin.py
@@ -1,28 +1,9 @@
 from django.contrib import admin
 
-from .models import (
-    Tournament,
-    TournamentParticipant,
-    MatchParticipant,
-    PongMatch,
-)
+from .models import GameMatch
 
 
-@admin.register(Tournament)
-class TournamentModelAdmin(admin.ModelAdmin):
+@admin.register(GameMatch)
+class GameMatchAdmin(admin.ModelAdmin):
     pass
 
-
-@admin.register(TournamentParticipant)
-class TournamentParticipantModelAdmin(admin.ModelAdmin):
-    pass
-
-
-@admin.register(MatchParticipant)
-class MatchParticipantModelAdmin(admin.ModelAdmin):
-    pass
-
-
-@admin.register(PongMatch)
-class PongMatchModelAdmin(admin.ModelAdmin):
-    pass
