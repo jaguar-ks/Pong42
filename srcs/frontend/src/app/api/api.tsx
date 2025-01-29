cc5e863e1bef70bc379ac06f9a682d597466ada0
ismailbarka <ismailbarka82@gmail.com>
2025-01-29 15:13:41 +0100

    deleting unused functions

--- srcs/frontend/src/app/api/api.tsx
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export const getCurrentUser = async () => {
-  const { data } = await axios.get(`${process.env.API_BASE_URI}${url}`, {
-    withCredentials: true
-  });
-  return data;
-};
