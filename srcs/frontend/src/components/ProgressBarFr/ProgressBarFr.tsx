5bee03233a6e1d01d9fa213695cc3b36bfb62cb3
ismailbarka <ismailbarka82@gmail.com>
2025-02-06 22:28:42 +0100

    fix progressBar Componenet

--- srcs/frontend/src/components/ProgressBarFr/ProgressBarFr.tsx
+++ srcs/frontend/src/components/ProgressBarFr/ProgressBarFr.tsx
@@ -1,6 +1,6 @@
 "use client"
 import React from 'react'
-import classes from './progressBarFr.module.css'
+import classes from './progressBar.module.css'
 
 const calculateLevel = (rating: number): number => {
   return Math.floor(rating / 100) + 1;
@@ -10,23 +10,22 @@ const calculatePercentage = (rating: number): number => {
   return rating % 100;
 }
 
-interface ProgressBarFrProps {
-  ratingFr: number;
-}
+const ProgressBarFr = ({ratingFr}) => {
 
-const ProgressBarFr: React.FC<ProgressBarFrProps> = ({ ratingFr }) => {
-  const rating = ratingFr || 0; 
+  const rating = ratingFr;
+  console.log("rating",rating);
   const level = calculateLevel(rating);
   const percentage = calculatePercentage(rating);
 
   return (
     <div className={classes.container}>
-      <div className={classes.Fr} style={{ width: `${percentage}%` }}></div>
+      <div className={classes.progressBar} style={{width: `${percentage}%`}}></div>
       <div className={classes.textContainer}>
-        <p className={classes.level}>Level {level} - {percentage}%</p>
+        <p className={classes.level}>Level {level} - {Math.round(percentage, 2)}%</p>
       </div>
     </div>
-  );
+  )
 }
 
-export default ProgressBarFr;
+export default ProgressBarFr
+
